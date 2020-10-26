6b60df59150840ed685ae1dc1da32a68cdee4ae8
angelodias <angelossdias@gmail.com>
2020-10-26 12:10:31 -0300

    Updating logout method and removing annotations routes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,16 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     * @param AuthenticationUtils $authenticationUtils
-     * @return Response
-     */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -28,11 +22,12 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
     public function logout()
     {
-//        throw new LogicException('TODO This method can be blank - it will be intercepted by the logout key on your firewall.');
+        if($this->container->has('session')) {
+            $session = $this->container->get('session');
+            $session->clear();
+        }
+
     }
 }
